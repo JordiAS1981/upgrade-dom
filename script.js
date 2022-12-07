dc9eb2e0b5fefc86164caa2da1e9824547ea92c7
Jordi Peña Bootcamp <116197945+JordiAS1981@users.noreply.github.com>
2022-12-07 21:52:53 +0100

    Itreracion#1

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1.1
+
+const boton = document.querySelector(".showme");
+console.log(boton);
+
+// 1.2
+
+const pillado = document.querySelector("#pillado");
+console.log(pillado);
+
+// 1.3
+
+const parrafos = document.querySelectorAll("p");
+console.log(parrafos);
+
+// 1.4
+
+const todosPokemon = document.querySelectorAll("pokemon");
+console.log(todosPokemon)
+
+// 1.5
+
+const 
