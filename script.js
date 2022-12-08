11c9ad675b6576d5fc0fe9f6e145d8e9ed324620
Jordi Peña Bootcamp <116197945+JordiAS1981@users.noreply.github.com>
2022-12-08 12:14:58 +0100

    Iteration#1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// 1.1
+// 1.1 Usa querySelector para mostrar por consola el boton con clase .showme
 
-const boton = document.querySelector(".showme");
-console.log(boton);
+const botonShowMe = document.querySelector(".showme");
+console.log(botonShowMe);
 
 // 1.2
 
@@ -15,9 +15,14 @@ console.log(parrafos);
 
 // 1.4
 
-const todosPokemon = document.querySelectorAll("pokemon");
+const todosPokemon = document.querySelectorAll(".pokemon");
 console.log(todosPokemon)
 
 // 1.5
 
-const 
+const todosTestMe = document.querySelectorAll('[data-function="testMe"]');
+console.log(todosTestMe);
+
+// 1.6 . innerText imprime el nombre de la posicion 3 
+
+console.log(todosTestMe[3].innerText);
